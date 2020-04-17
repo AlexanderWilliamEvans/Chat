60ccfd9ddc7f59f6ff955f37962037c78cd370c2
Alexander Evans <ae.evans70@gmail.com>
2020-04-17 07:31:12 +0200

    first commit

---
+++ Chat/Client.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace Chat
+{
+    public class Client
+    {
+        public Client()
+        {
+            StartClient();
+
+        }
+        /// <summary>
+        /// Starts a client which connects to the server
+        /// </summary>
+        public void StartClient()
+        {
+            try{
+                TcpClient tcpClient = new TcpClient("127.0.0.1", 5000);
+                Console.WriteLine("Connected to server.");
+
+                Thread thread = new Thread(HandleMessages);
+                thread.Start(tcpClient);
+
+                StreamWriter sWriter = new StreamWriter(tcpClient.GetStream());
+
+                while (true)
+                {
+                    if (tcpClient.Connected)
+                    {
+                        string input = Console.ReadLine();
+                        sWriter.WriteLine(input);
+                        sWriter.Flush();
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+
+            Console.ReadKey();
+
+        }
+        /// <summary>
+        /// Send message via a socket
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <param name="message"></param>
+        static void HandleMessages(object obj)
+        {
+            TcpClient tcpClient = (TcpClient)obj;
+            StreamReader sReader = new StreamReader(tcpClient.GetStream());
+
+            while (true)
+            {
+                try
+                {
+                    string message = sReader.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Chat/Server.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace Chat
+{
+    public class Server
+    {
+        IPAddress ipAddress;
+        int port;
+        Socket listenerSocket;
+        TcpListener server;
+        TcpClient client;
+        List<TcpClient> clients;
+        NetworkStream stream;
+
+        public Server()
+        {
+            this.ipAddress = IPAddress.Parse("127.0.0.1");
+            this.port = 666;
+            StartServer();
+        }
+        /// <summary>
+        /// Setup a server socket which the clients can connect to
+        /// </summary>
+        private void StartServer()
+        {
+            //listenerSocket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            //listenerSocket.Bind(new IPEndPoint(ipAddress, port));
+            //listenerSocket.Listen(100);
+
+            //listenerSocket.BeginAccept(OnClientConnected, null);
+            server = new TcpListener(IPAddress.Parse("127.0.0.1"), 6500);
+            clients = new List<TcpClient>();
+            server.Start();
+            Debug.WriteLine("Connected!");
+
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+                AddClient(client);
+                Debug.WriteLine("New client has connect!");
+                Debug.WriteLine("Current number of clients connected: " + clients.Count);
+                stream = client.GetStream();
+                Byte[] bytes = new Byte[1048];
+                int k = stream.Read(bytes, 0, bytes.Length);
+                string data = null;
+                int i;
+                // Loop to receive all the data sent by the client.
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    data = Encoding.ASCII.GetString(bytes, 0, i);
+                    Debug.WriteLine("Received: {0}", data);
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+                    stream.Write(msg, 0, msg.Length);
+                    Debug.WriteLine("Sent: {0}", data);
+                }
+
+            }
+        }
+
+        private List <TcpClient> AddClient(TcpClient client)
+        {
+            clients.Add(client);
+            return clients;
+        }
+
+        /// <summary>
+        /// When a client connects to the server, the socket used by the client is connected on the server side
+        /// </summary>
+        /// <param name="asyncResult"></param>
+        private void OnClientConnected(IAsyncResult asyncResult)
+        {
+            var listenerSocket = (Socket)asyncResult.AsyncState;
+            var clientSocket = listenerSocket.EndAccept(asyncResult);
+        }
+        /// <summary>
+        /// Common method for how to start receiving messages
+        /// </summary>
+        /// <param name="socket"></param>
+        private void ReceiveMessage(Socket socket)
+        {
+            byte [] buffer = new byte[2048];
+            socket.BeginReceive(buffer, 0, 2048, 0, OnMessageReceived, socket);
+        }
+        /// <summary>
+        /// When a new message is received, this method is triggered to extract the message
+        /// </summary>
+        /// <param name="result"></param>
+        private void OnMessageReceived(IAsyncResult result)
+        {
+            var socket = (Socket)result.AsyncState;
+            var messageLength = socket.EndReceive(result);
+            var buffer = new byte[2048];
+            var message = Encoding.UTF8.GetString(buffer, 0, messageLength);
+        }
+    }
+}
